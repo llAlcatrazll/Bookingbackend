527e62bf1fcced4f2a3bdb1c89b1b6da888defc6
llAlcatrazll <alexismagaway456@gmail.com>
2023-12-14 20:10:16 +0800

    log in messagge

--- socket.js
+++ socket.js
@@ -17,23 +17,32 @@
 //   console.log("server is running");
 // });
 const express = require("express");
-const { createServer } = require("http");
-const { Server } = require("socket.io");
-
+const cors = require("cors");
 const app = express();
-const httpServer = createServer(app);
-const io = new Server(httpServer, {
-  /* options */
-  cors: { origin: "*" },
+
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// io.on("connection", (socket) => {
+//   // ...
+//   socket.on("blog", (socket) => {
+//     // socket.emit("receive", socket);
+//   });
+// });
+const emailUseradmin = "Alcatraz";
+const passwordUseradmin = "123";
+app.post("/login", (req, res) => {
+  console.log(req.body);
+  const { emailUser, passwordUser } = req.body;
+
+  if (emailUser !== emailUseradmin) {
+    console.log("Email is fine");
+    res.status(404).send("Cant Find that");
+    return;
+  }
+  res.status(200).send({ message: "You have logged in" });
 });
 
-io.on("connection", (socket) => {
-  // ...
-  socket.on("blog", (socket) => {
-    // socket.emit("receive", socket);
-  });
-});
-
-httpServer.listen(3000, () => {
+app.listen(3000, () => {
   console.log("Server is listening");
 });
